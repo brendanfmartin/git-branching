c2d60c17d9eee1761150586dce6e175bd73e4f5b
Brendan Martin <brendanfmartin@me.com>
2018-04-11 13:47:31 -0400

    Bug/hotfix feature 1 - QA-1 (#2)
    * FF-01 - Feature 1 is totes done (#1)
    
    * QA-1 - fixing text for feature 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  isOneShown = false;
   title = 'git branching practice';
+
+  toggleOne(): void {
+    this.isOneShown = !this.isOneShown;
+  }
 }
